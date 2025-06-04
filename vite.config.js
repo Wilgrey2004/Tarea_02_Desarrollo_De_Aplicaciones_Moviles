818e52762697139c3ee400046af8587eedd55fd5
Wilgrey Ravelo Cruz <167430214+Wilgrey2004@users.noreply.github.com>
2025-06-04 16:15:48 -0400

    dist agregado

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+
+  base: "hhtps://Wilgrey2004.github.io/Tarea_02_Desarrollo_De_Aplicaciones_Moviles",
+});
